d30dce4f5d4f6ddbaad98bf31b092d0e7164ca87
mbauerTgm <144990660+mbauerTgm@users.noreply.github.com>
2026-01-06 19:01:48 +0100

    [EK] fixed global filtering
    Co-authored-by: MateiDragne <MateiDragne@users.noreply.github.com>

--- src/main/java/com/mbauer_mdragne/vue_crud/DTOs/AnalysisGlobalFilterDto.java
+++ src/main/java/com/mbauer_mdragne/vue_crud/DTOs/AnalysisGlobalFilterDto.java
@@ -3,9 +3,12 @@ package com.mbauer_mdragne.vue_crud.DTOs;
 import lombok.Data;
 import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Data
 public class AnalysisGlobalFilterDto {
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Range<LocalDateTime> globalDateIn;
 
 }
