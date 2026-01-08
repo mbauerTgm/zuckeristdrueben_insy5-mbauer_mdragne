dc03f19aed9073943d5e96440479a8b99bd67092
mbauerTgm <mbauer@student.tgm.ac.at>
2026-01-08 14:08:23 +0100

    [EK] improved backup volume management

--- web/frontend/cypress/e2e/Analysis.cy.js
+++ web/frontend/cypress/e2e/Analysis.cy.js
@@ -221,30 +221,6 @@ describe('Analysis Test:', () => {
   })
 
   it('Check create required error', () => {
-    cy.get(':nth-child(1) > select').select('Analysis')
-    //Daten Laden
-    cy.get('.btn-load').click()
-    //neuer Eintrag
-    cy.get('.btn-save').click()
-    //Save btn
-    cy.get('.form-actions > .btn-save').click()
-    cy.get('.error-text').should('contain', 'Alle Pflichtfelder müssen ausgefüllt sein')
-    cy.get('.error-text').should('contain', 'Fehlende Felder: - S id')
-  })
-
-  it('Check create required error', () => {
-    cy.get(':nth-child(1) > select').select('Analysis')
-    //Daten Laden
-    cy.get('.btn-load').click()
-    //neuer Eintrag
-    cy.get('.btn-save').click()
-    //Save btn
-    cy.get('.form-actions > .btn-save').click()
-    cy.get('.error-text').should('contain', 'Alle Pflichtfelder müssen ausgefüllt sein')
-    cy.get('.error-text').should('contain', 'Fehlende Felder: - S id')
-  })
-
-  it('Check create required error', () => {
     
     cy.get(':nth-child(1) > select').select('Analysis')
     //Daten Laden
